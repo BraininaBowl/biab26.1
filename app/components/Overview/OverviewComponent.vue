89125612f453d27ac39c10ce8c16307e599c76ec
BraininaBowl <fransvb@gmail.com>
2026-06-10 23:07:46 +0200

    Fix item load error

--- app/components/Overview/OverviewComponent.vue
+++ app/components/Overview/OverviewComponent.vue
@@ -9,54 +9,21 @@
 </template>
 
 <script setup>
-const items = new Array();
-const filters = [{ attribute: "trashed", values: [false, undefined] }]
-const parsed = true;
-const params = {filters: filters, parsed: parsed};
-const { data, status, error, refresh, clear } = await useFetch(
-  "/api/items/all",
-  {
-    query: {filters: JSON.stringify(filters), parseHTML: true}
-  },
-  {
-    onRequestError({ request, options, error }) {
-      // Handle the request errors
-    },
-    onResponse({ request, response, options }) {
-      // response._data.data.items.sort((a, b) => new Date(a.date) - new Date(b.date));
-      // filters.forEach((filterItem) => {
-      //   response._data.data.items = response._data.data.items.filter((el) =>
-      //     filterItem.values.includes(el[filterItem.attribute]),
-      //   );
-      // });
+const { item, items, fetchItem, fetchItems, status } = useItems();
 
-      response._data.data.items.forEach((item) => {
-        // item.description = toHtml(item.description);
-        items.push(item);
-      });
-      
-    },
-    onResponseError({ request, response, options }) {
-      // Handle the response errors
-    },
-  },
+fetchItems([{ attribute: "trashed", values: [false, undefined] }]).then(
+  resizeItems,
 );
-
-// fetchItems([{attribute: "trashed", values: [false, undefined]},]).then(resizeItems);
 function resizeItems() {
-  for (const item of items) {
-    const domItem = document.getElementById(`card_${item.id}`);
-    if (domItem) {
-      let height = domItem.querySelector(".card").clientHeight + 64;
-      domItem.style.gridRow = "auto / span " + height;
-      domItem.style.opacity = 1;
-    }
-  }
+  document.querySelectorAll(".card_wrapper").forEach((domItem) => {
+    let height = domItem.querySelector(".card").clientHeight + 64;
+    domItem.style.gridRow = "auto / span " + height;
+    domItem.style.opacity = 1;
+  });
 }
 
 onMounted(() => {
   window.addEventListener("resize", resizeItems());
-  resizeItems();
 });
 onUnmounted(() => {});
 </script>
--- server/api/items/all.get.js
+++ server/api/items/all.get.js
@@ -1,21 +1,10 @@
 export default defineEventHandler(async (event) => {
-  const params = getQuery(event);
   const storage = useStorage("itemStore");
   const keys = await storage.keys();
   const promises = keys.map((key) => storage.getItem(key));
   const allItems = await Promise.all(promises);
   const items = allItems;
 
-  // modify data
-  const filters = JSON.parse(params.filters)
-  console.log("filters", typeof(filters), filters)
-
-        // response._data.data.items.sort((a, b) => new Date(a.date) - new Date(b.date));
-      // filters.forEach((filterItem) => {
-      //   response._data.data.items = response._data.data.items.filter((el) =>
-      //     filterItem.values.includes(el[filterItem.attribute]),
-      //   );
-      // });
 
   return {
     status: "success",
