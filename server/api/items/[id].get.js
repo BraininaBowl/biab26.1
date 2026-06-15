17ad83ea3002722821928ff432b5f7c3ab4f54f4
BraininaBowl <fransvb@gmail.com>
2026-06-15 23:29:25 +0200

    Start setting up image upload

--- server/api/items/[id].get.js
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const storage = useStorage("itemStore");
-    const id = getRouterParam(event, "id");
-    const item = await storage.getItem(id + ".json");
-    return {
-      status: "succes",
-      data: { item },
-    };
-  });
---
+++ server/api/items/file.upload.js
@@ -0,0 +1,8 @@
+export default defineEventHandler(async (event) => {});
+
+const formData = await readMultipartFormData(event);
+if (!formData || formData.length === 0) {
+  throw createError({
+    statusCode: 400,    statusMessage: "No files uploaded",  });
+}
+const file = formData[0];
