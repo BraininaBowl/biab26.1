71c94a60b92c6af8aba4062c291e9a69e81074cb
BraininaBowl <fransvb@gmail.com>
2026-09-08 08:22:51 +0200

    Init children overview

---
+++ app/components/Overview/OverviewChildrenComponent.vue
@@ -0,0 +1,22 @@
+<template></template>
+
+<script setup>
+const { fetchStatelessItems } = useItems();
+const props = defineProps({
+	id: {
+		type: string,
+		required: true,
+	},
+});
+
+const id = props.id || [];
+const filters = [
+	{ attribute: "trashed", values: [false, undefined] },
+	{ attribute: "hidden", values: [false, undefined] },
+	{ attribute: "parent", values: [id] },
+];
+const childItems = await fetchStatelessItems(filters);
+console.log("childItems", childItems)
+</script>
+
+<style type="css" scoped></style>
